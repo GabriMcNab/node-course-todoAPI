7dcd2c0f73f55ff99117e5e61be27e8c99c3afb1
Gabriele Fazio <Gabriele@MBP-di-Gabriele.homenet.telecomitalia.it>
2018-10-16 16:43:44 +0200

    Add POST /user route and token method

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+//TODOS ROUTES
+
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
@@ -84,6 +86,19 @@ app.patch('/todos/:id', (req, res) => {
     }
     res.send({ todo });
   }).catch((e) => res.status(400).send());
+});
+
+//USERS ROUTES
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => res.status(400).send(e))
 });
 
 app.listen(port, () => {
