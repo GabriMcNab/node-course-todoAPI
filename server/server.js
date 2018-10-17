5add1889b0d2d2810ff92586f20c5d02d90e0df6
Gabriele Fazio <Gabriele@MBP-di-Gabriele.homenet.telecomitalia.it>
2018-10-17 13:30:09 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user)
+    res.header('x-auth', token).send(user);
   }).catch((e) => res.status(400).send(e))
 });
 
@@ -109,6 +109,16 @@ app.listen(port, () => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => res.status(400).send());
 });
 
 module.exports = { app }
